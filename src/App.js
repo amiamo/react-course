483a00dda2d5e4984d4a70f29001f833157f1e20
amiamo <amiamo@gmail.com>
2024-08-24 22:42:46 +0900

    useeffect_cleanup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
-import Button from "./Button"
-import styles from "./App.module.css"
 import { useEffect, useState } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] =useState("")
-  const onChange = (event) => setKeyword(event.target.value)
-  const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time")
+function Hello(){
   useEffect(() => {
-    console.log("i run only once")
+    console.log("created")
+    return () => console.log("destroyed")
   },[])
-  useEffect(() => {
-    console.log("i run when 'keyword' changes")},[keyword])
+  return <h1>Hello</h1>
+}
 
-  useEffect(() => {
-    console.log("i run when 'counter' changes")
-  },[counter])
-  useEffect(() => {
-    console.log("i run when keyword & counter changes")
-  },[keyword, counter])
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing(prev => !prev)
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."></input>
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
